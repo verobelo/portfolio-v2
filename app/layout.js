1e6c39e02eedd67972efedf75af1fef7a73fd1af
verobelo <142001535+verobelo@users.noreply.github.com>
2025-08-29 17:47:05 +0200

    Test og image

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
     siteName: 'Veronika Belozerova Portfolio',
     images: [
       {
-        url: 'https://veronikacodes.com/og-image.jpg',
+        url: 'https://portfolio-v2-brown-omega.vercel.app/og-image.jpg',
         width: 1200,
         height: 630,
         alt: 'Veronika Belozerova - Customer-Centric React Developer',
