62d810154ee13d09974f5db4e296d279cfb49b51
verobelo <142001535+verobelo@users.noreply.github.com>
2025-08-30 02:11:24 +0200

    Change og image

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
     siteName: 'Veronika Belozerova Portfolio',
     images: [
       {
-        url: 'https://veronikacodes.com/og-image.png',
+        url: 'https://veronikacodes.com/og-image1.png',
         width: 1200,
         height: 630,
         alt: 'Veronika Belozerova - Customer-Centric React Developer',
